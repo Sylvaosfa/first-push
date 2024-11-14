2043a2d0ea3c4deb89aae8546dc7c6fb99256ee2
Sylvaosfa <sylvaosfawrites@gmail.com>
2024-11-14 05:57:45 -0800

    first work

---
+++ javascript/index.js
@@ -0,0 +1,710 @@
+// console.log(34344798)
+// alert(67811374878)
+// prompt("67342398")
+
+// console.log('my name is osfa')
+// console.log("my pet's name is osfa")
+// console.log(58 + 8)
+// console.log(true)
+
+// var myName="Var: My name is Osfa and i love coding"
+// console.log(myName)
+
+// let greeting = "let: Hello there"
+// console.log(greeting)
+
+// const greet = "const: Good morning"
+// console.log(greet)
+
+// let person = prompt ("please enter your name")
+// console.log(person)
+
+// { var x = 2 }
+// //string concatenation
+
+
+ const firstname = "bliss"
+// const middlename = "bella"
+// const lastname = "bona"
+// const num = 6578
+// const num2 = 6543
+// firstname.concat(middlename, lastname)
+// console.log(firstname.concat(" ", middlename," ", lastname, " ", num," ", num2))
+
+// //string length
+// console.log(firstname.length)
+// console.log(middlename.indexOf('a'))
+// console.log(lastname[3])
+
+// //string slice
+// console.log( middlename.slice(1,3))
+// console.log(lastname.slice(0,2))
+
+
+// var firstName ="jerry"
+// var lasttName ="jemma"
+// var middleName ="pearl"
+// var nos = 4537
+// var nos2 = 4511
+// console.log( firstName + " " +  middleName + lasttName + " " + nos + " " + nos2 )
+
+
+// const longestname ="pneumonoultramicroscopicsilicovolcanoconiosis"
+// console.log( longestname[0] + longestname[14] + longestname[1] + longestname[2] )
+
+// var bgName ="pneumonoultramicroscopicsilicovolcanoconiosis"
+// console.log( bgName[18] + bgName[15] + bgName[12] + bgName[0  ])
+
+// //string template literals
+// const promn = prompt("what is your name")
+// console.log(`My name is ${promn}`)
+
+// //Arithimetic operators
+// //Addition(+)
+// //subtraction(-)
+// //Multiplication(*)
+// //Division(/)
+// //Expoonential(**)
+// //modulus(%)
+
+// //Increment(++)
+// let n=5
+// n++
+// console.log(n)
+
+// let m = 4
+// m+=6
+// console.log(m)
+
+// let z = 6
+// z++
+// console.log(z)
+
+// //Decreement(--)
+// let p=5
+// p--
+// console.log(p)
+
+// let u = 7
+// u-=8
+// console.log(u)
+
+// //Random Number
+// //How to generate random numbers in java script
+
+
+// Math.floor()
+
+// console.log(Math.floor(Math.random()*8))
+
+// Math.ceil()
+
+// //Decision making
+// const condition =10
+// if (condition >=4 ) {
+//     console.log("N is not a number")
+// }
+// else if( condition > 4 && condition < 14 ) {
+//     console.log("N is a number less than 14")
+// }
+
+// let time =  prompt ("what is the time")
+
+// if( time < 12 ) {
+    
+//     console.log("Good morning")
+// }
+
+// else if  ( time >12 && time<18) {
+//     console.log("Good evening")
+// }
+
+// else if( time >18 && time <24) {
+//     console.log("Good night")
+// }
+// else{
+//    console.log ("this time does not exist")
+// }
+
+// //condition ternary
+// console.log (condition > 4 ? "N is a number" : "N is not a number")
+
+
+// var rando = (Math.ceil(Math.random()*1000000))
+// alert(rando)
+// let number = prompt ("input one time code here")
+
+// if( number == rando) {
+//     console.log("login successful")
+// }
+// else{
+//     console.log("sorry, you don't have access to this website")
+// }
+
+// //logical operators
+// //and , or , not
+
+// const vivian = true
+// const lina = false
+// const tess = true
+
+// if(vivian && lina && tess){
+//     console.log("All are true")
+// }
+// else{
+//     console.log("one is stubborn")
+// }
+
+// if(vivian || lina || tess){
+//     console.log("All are true")
+// }
+// else{
+//     console.log("one is stubborn")
+// }
+
+// //Array - collectiion of multiple data into a single variable. The array is denoted using the square bracket[]
+
+// const arr = [ "Becca", "Paula", "laura", 655]
+// console.log(arr)
+// arr[1] = "vivian"
+// arr[4] = "Lagos"
+// arr[5] ="Pelunmi"
+// arr[1] = "null"
+
+
+
+// const ray = ["ball", "car","567", "tank", "loafer"]
+// console.log(ray[0])
+// console.log(ray)
+// console.log (ray[2])
+
+// // Array-pop : deleting last string
+// var bread = ["sardine", "burger"]
+// bread.pop()
+// console.log(bread)
+// // Array-push :
+// const fruits = ["banana", "apple", "mango", "peear", "berry"]
+// fruits.push("lemon ")
+// console.log(fruits)
+
+// // Array-shift
+// const house = ["bungalow", "duplex", "storey building"]
+// house.shift()
+// console.log(house)
+// //Array-unshift
+// var cake = ["vanilla", "chocolate"]
+// cake.unshift("strawberry")
+// console.log(cake)
+
+// //Array-splice
+// const car =["volvo", "lexus", "camry", "benz"]
+// car.splice(1,2, "fumatic", "toyota")
+// console.log(car)
+// //Array-slice
+// var items =["bag", "jewelry", "clothes", "bedsheet"]
+// var fru = items.slice(1,2)
+// console.log(fru)
+
+// //Array reverse
+// const book =["dweller", "obstacles", "desires", "obsessed"]
+// book.reverse()
+// console.log(book)
+
+// //Array include
+// var play = ["pool", "snooker", "board game", "monopoly"]
+// play.fru = play.includes("pool", 3)
+// console.log(play)
+
+// //PRACTICE
+//  var carName = "volvo"
+//  console.log(carName)
+//  let q = 50
+//  console.log(q)
+//  //display the sum of 5+10, using two variables s and t
+
+//  let s = 25;
+//  let t = 10;
+//  console.log(s+t)
+
+
+//  let w = 5;
+//  w+=10;
+ 
+//  console.log(w)
+
+// let water = prompt("House list")
+// console.log(`The house list entails things like  the  following: ${water}`)
+
+//  //Revision
+// console.log(Math.random())
+// console.log(Math.floor(Math.random()) *1000000 )
+// console.log(Math.ceil(Math.random()*9))
+
+// //javascript objects : its symbolized using the {}
+// const persone = {
+//     namee: "David",
+//     age: "23",
+//     address: "Rivers state",
+//     adult: "false",
+// }
+
+// persone.namee = "steven"
+// persone.age = "54"
+// console.log(persone.namee)
+// console.log(persone.age)
+// console.log(persone.address)
+ 
+// if (persone.adult ===true) {
+//     console.log ("david is an adult")
+// }
+// else {
+//     console.log ("david is not an adult")
+// }
+
+// //nested object
+// const cars = {
+//    car1 :{
+//     make: "Cyber Truck",
+//     model: "2020 model",
+//     speed: "240km/h",
+//     },
+//    car2 :{
+//     make: "benz",
+//     model: "2024 model",
+//     speed: "240km/h",
+//     },
+//    car3 :{
+//     make: "Bugati",
+//     model: "2017 model",
+//     speed: "280km/h",
+//     },
+//    car4 :{
+//     make: "Lexus",
+//     model: "2026 model",
+//     speed: "270km/h",
+//     },
+// }
+// console.log(cars.car1.make)
+// console.log(cars.car2.make)
+// console.log(cars.car3.make)
+// console.log(cars.car4.make)
+
+// //inside array of objec[]
+// const carss = [
+//    ca1 ={  
+//     make: "Truck",
+//     model: "2020 model",
+//     speed: "240km/h",
+//    },
+//    ca2 ={  
+//     make: "leumosine",
+//     model: "2024 model",
+//     speed: "240km/h",
+//    },
+//    ca3 ={   
+//     make: "honda",
+//     model: "2017 model",
+//     speed: "280km/h",
+//   },
+//    ca4 ={   
+//     make: "camry",
+//     model: "2026 model",
+//     speed: "270km/h",
+//    },
+//    ]
+
+//  console.log(carss[0].make)
+//  console.log(carss[1])
+//  console.log(carss[2].speed)
+//  console.log(carss[3])
+
+//  //javascript loops
+
+//  for(k = 0; k < 10; k++){
+//     console.log(k)
+//  }
+
+//  for(q = 16; q < 20; q++){
+//     console.log(q)
+//  }
+
+// for(i = 0; i < carss.length; i++) {
+//     console.log(carss[i])
+// }
+
+// const job = {
+//     jname:"cooper",
+//     location: "choba",
+//     aim: "blogwriting",
+//     address: "Rivers state",
+// }
+
+// console.log(job.jname)
+// console.log(job.location)
+// console.log(job.aim)
+// console.log(job.address)
+
+// for( const key in job ) {
+//     console.log(`${key} : ${job[key]}`)
+// }
+
+// const range = {
+//     range1 :{
+//      make: "power",
+//      kind: "holla",
+//      mole: "bim",
+//      },
+//     range2 :{
+//      make: "authority",
+//      kind: "ello",
+//      mole: "lindy",
+//      },
+//     range3 :{
+//      make: "Bugati",
+//      kind: "hi",
+//      mole: "seph",
+//      },
+//  }
+
+//  console.log(range.range1.make)
+//  console.log(range.range2.kind)
+//  console.log(range.range3.mole)
+ 
+
+
+// //while loop; when u not sure of the variable
+// let v = 2
+// while(v < 20) {
+//     console.log(v);
+//     v++;
+// }
+
+// //Functions in javascript: block of code that  perform a particular task
+// function addnumber() {
+//     console.log(3 + 5)
+//     console.log("This is a function")
+//     alert("my function")
+// } 
+// addnumber();
+
+//DOM :selecting html and making changes with it
+// const header = document.getElementById("hd")
+// const button = document.getElementById("btn")
+// const button1 = document.getElementById("rtn")
+
+// console.log(header)
+// console.log(button)
+// console.log(button1)
+
+
+//header.textContent = 70
+
+// button.addEventListener("click", function(){
+//     header.textContent++
+// })
+
+// button1.addEventListener("click", function(){
+//     header.textContent--
+// }) 
+
+// //parameters: used as placeholders
+
+// function multiply(a,b){
+//     console.log(a+b)
+
+// }
+// //arguments: when u use the exact value in place of the parameters
+// multiply(2,5)
+
+//create a function that adds  two numbers
+//  const hd = document.getElementById("hd")
+//  const hd2 = document.getElementById("hd2")
+//  const hd3 = document.getElementById("hd3")
+
+//  hd3.textContent = 0
+//  function add(a,b) {
+//     hd.textContent =a
+//      hd2.textContent =b
+//      hd3.textContent =a + b;
+//  }
+
+//  add(9,5)
+
+const hd = document.getElementById("hd")
+const d2 = document.getElementById("d2") 
+const d3 = document.getElementById("d3")
+const btn4 = document.getElementById("btn4")
+const btn5 = document.getElementById("btn5")
+
+function addNumbers(c,d) {
+    hd.textContent =c
+    d2.textContent =d
+    d3.textContent = c + d
+      console.log(c+d)
+}
+
+function subtractNumbers(c,d) {
+    hd.textContent =c
+    d2.textContent =d
+    d3.textContent =c - d
+     console.log(c-d)
+
+}
+
+btn4.addEventListener("click", function(){
+    addNumbers(5, 5)
+})
+btn5.addEventListener("click", function(){
+    subtractNumbers(8,6)
+})
+
+
+//high order function- are function that accepts another function as an arguements
+//H.O.F
+function createNumber(cal) {
+    console.log("Hello")
+    return cal();
+}
+
+//arguement
+function anotherfunction(){
+      console.log("call back we are learning")
+}
+
+createNumber(anotherfunction)
+
+//variable scope in javascript
+const variable = "hello"
+
+function firstName() {
+    console.log(firstname)
+}
+
+function greetName() {
+    const time = 12
+    if(time <= 12){
+        var timee2 = 23
+    }
+     else{
+        console.log(timee2 + 1)
+         console.log("No greet")
+     }
+    console.log(timee2)
+}  
+greetName();
+
+//arrow function - can take parameters or arguement
+//const fnxName = () => {
+
+//}   
+//impicit
+const fnxName = () => console.log("Arrow function")
+fnxName();
+
+const classcrew = [
+        {name:"Joy", desc: "she is tall and beautiful"},
+        {name:"Jaima", desc: "she is smart and beautiful"},
+        {name:"shally", desc: "He is pale and loving"},
+       {name:"plrew", desc: "He is tall and exclusive"},
+       {name:"jane", desc: "she is organized and neat"},
+      {name:"Jade", desc: "Hee is caring and bold"},
+]
+
+
+
+
+//Recursive Function in javascript: a function that calls itself, until the arguement is met
+//recursive condition/ state- what tells us when the function should stop running
+//Base condition- 
+
+function recure(a){
+   if(a <= 0){
+    console.log("Done")
+     return;
+  }
+   console.log(a)
+   recure(a - 1)
+}
+  recure(7);
+
+  const someArray = [ 2, 4, 5, 6, 9]
+
+  function sumofArray(n){
+    if (n.length=== 0){
+        console.log("bravo!!")
+        return 0;
+    }
+
+    console.log(n)
+    return n[0] + sumofArray(n.slice(1))
+  }
+  console.log (sumofArray(someArray))
+
+  //methods in javascript
+  const individuals = {
+     firstName: "jedidiah",
+   
+     HiJedidiah: function Hi(){
+        console.log(this.HiJedidiah)
+     }
+   }
+
+  const fruits = [ "mango", "orange", "pineapple", "indomie", "watermelon"]
+  //for each takes 3 properties : element,index,array
+
+  fruits.forEach(function(elem, dex, ray){
+    console.log(elem)
+    console.log(dex)
+    console.log(ray)
+    console.log(`${elem} is at index of ${dex} in the ${ray} array`)
+  })
+
+  const products = [
+    {name: "Laptop", price: 1000},
+    {name: "phone", price: 600},
+    {name: "tablet", price: 400},
+  ];
+
+  products.forEach(function(eachItem){
+    eachItem.discountedPrice = eachItem.price * 0.5
+    console.log(`the price for ${eachItem.name} is ${eachItem.price} but the discounted price is ${eachItem.discountedPrice}`)
+  })
+  console.log(products)
+
+  
+//map function
+//map method: a javascript function that iterates over an array an execute a provided function on each of the element. it does not modify the array
+
+// fruits.map(function(element, dex, ray){
+//   console.log(element + 1)
+// })
+const frut = fruits.forEach(function(element, dex, ray){
+  return element;
+})
+console.log(frut);
+console.log(fruits);
+
+classcrew.map((element, index, array) => {
+  console.log(element.name)
+})
+
+//find function : an array function that checks for first element that stisfies a condition  
+
+const num = [ 2, 6, 5, 3, 7,10]
+
+const fin = num.find(function(element, index, array){
+  return element > 5
+})
+console.log(fin)
+
+//filter: chcks all element thst satisfies a condition
+const fil = num.filter(function(element, index, array){
+  return element > 5
+})
+console.log(fil)
+
+// const ask = prompt("search for name")
+// const fill = classcrew.filter(function(element){
+//   if(ask === element.name){
+//     return element.desc;
+//   }
+// })
+// console.log(fill)
+//just making the description to show
+const ask = prompt("search for name")
+const fill = classcrew.filter(function(element){
+  if(ask === element.name){
+     //return element.desc;
+     console.log(element.desc)
+  }
+})
+console.log(fill)
+
+//rest params: used in unknown cases or unknown arguement
+
+function para(...re){
+      console.log(...re)
+}
+para(2, 5, 6, 5, 84, 38)
+
+function yam(a, b){
+  console.log(a,b)
+}
+yam(2, 9, 3)
+
+ const num2 = [5, 6, 2, 4, 7]
+ const numb = [12, 3, 9, 45,]
+console.log(...num2, ...numb)
+
+//Array destructuring
+const vehicle = ['mustang', 'f-150', 'expedition'];
+
+//old way
+console.log(vehicle[0])
+console.log(vehicle[1])
+console.log(vehicle[2])
+//end of oldway
+
+//destructuring
+const [aa, bb, cc] = vehicle
+console.log(aa)
+console.log(cc)
+console.log(bb)
+
+//object destructuring
+const crew = {
+    name:"Benita",
+    desc:"she is sick"
+}
+const {name, desc} = crew
+console.log(name)
+console.log(desc)
+
+
+const user = {
+  id:1,
+  profile: {
+    username: "johndoe",
+    location1: "New York"
+  }
+};
+// const {id, profile} = user
+// console.log(id)
+// console.log(profile)
+
+const {id, profile} = user;
+const {username, location1} = profile;
+console.log(id)
+console.log(username)
+console.log(location1)
+
+//destructuring params
+//without destructuring 
+function greet(user){
+  return `Hello ${user.name} your age is ${user.age}`
+}
+const use = greet({name: "Vivian", age:20})
+console.log(use)
+
+//with destructuring 
+function greet2({name, age}){
+  return `Hello ${name} your age is ${age}`
+}
+const use2 = greet2({name: "ViCKY", age:30})
+console.log(use2)
+
+//default params- giving parameters their value
+function greeter(name = "valerie", age = 19){
+  return `Hello ${name} your age is ${age}`
+}
+console.log(greeter())
+
+//Reduce function: a javascript method that iterates through each element in an array and applies a callback fuction, accumulating a single output. mostly used for summing array and flattening array etc
+
+const bull = [2, 4, 6, 7]
+
+const bull2 = bull.reduce(function(accumulator, currentvalue){
+  return accumulator +    currentvalue
+}, 1)
+
+console.log(bull2)
